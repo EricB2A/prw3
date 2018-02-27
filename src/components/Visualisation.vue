c6b19ade5bdf273d16421a7126cb37a38411b15b
Eric Broutba <eric.bousbaa@hotmail.com>
2018-02-27 14:44:46 +0100

    First visualisation done !

--- src/components/Visualisation.vue
+++ src/components/Visualisation.vue
@@ -1,15 +1,50 @@
-<template>
-  <div class="app">
-      <h1>Visualisation of your activity data</h1>
 
-  </div>
-</template>
 
 <script>
+function restructure(data_object) {
+    // change the structure of object to adapt to charjs as
+    /*
+        {
+          labels: ['January', 'February'],
+          datasets: [
+            {
+              label: 'GitHub Commits',
+              backgroundColor: '#f87979',
+              data: [40, 20]
+            }
+          ]
+        }
+    */
+    let new_structure = {};
+    new_structure.labels = [];
+    new_structure.datasets = [];
+    new_structure.datasets[0] = {};
+    new_structure.datasets[0].label = 'Activity data';
+    new_structure.datasets[0].backgroundColor = '#f87979';
+    new_structure.datasets[0].data = [];
+    for(let key in data_object) { // looping through our big boy
+        if(data_object.hasOwnProperty(key)) {
+            //
+            new_structure.labels.push(key);
+            new_structure.datasets[0].data.push(data_object[key]);
+        }
+    }
+    return new_structure;
+}
 // LineChart.js
 import { Line, Bar } from 'vue-chartjs'
-import activityData from '../../data/processed/processed.json'
+import activity_data from '../../data/processed/processed.json'
 
-console.log("ok");
-console.log(activityData)
+const data_scrutured = restructure(activity_data);
+console.log(data_scrutured);
+
+export default {
+  extends: Line,
+  props: ['data', 'options'],
+  mounted () {
+    this.renderChart(data_scrutured, this.options)
+  }
+}
+
+
 </script>
